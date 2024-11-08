1a3e97788a6f61bfc91f085d296b50e5a64ceaa3
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-11-08 19:30:41 +0530

    Add Dashboard and Slider components with appointment booking functionality

--- src/components/userpage/Dashboard.jsx
+++ src/components/userpage/Dashboard.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import Appbar from "./Appbar";
+import Slider from "./Slider";
+import { Typography } from "@mui/material";
+
 
 export default function Dashboard() {
     return(
-        <Appbar/>
+    <>
+    <Appbar/>
+    
+    <div className=' mt-5'>
+    <Typography className="text-center" variant='h4'>Book Appointments</Typography>
+    <div className="d-flex justify-content-center mt-4 gap-5">
+    <Slider/>
+    <Slider/>
+    <Slider/>
+    </div>
+    </div>
+    </>
+
     )
 }
--- src/components/userpage/Slider.jsx
+++ src/components/userpage/Slider.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import { Button } from '@mui/material';
 
 export default function Slider() {
-    
+  return (
+    <Card sx={{ maxWidth: 340 }}>
+      <CardActionArea className='d-flex'>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://cdn.pixabay.com/photo/2017/01/31/03/23/pointer-2022862_1280.png"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Dr. JohnSon
+          </Typography>
+          <Typography>MBBS - General Medicine</Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Apollo Hospital - Chennai
+            Experianced - 5 years
+          </Typography>
+          <Button variant='contained'>Book</Button>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
