5824c2e871df36bd4ec8e53648e343646213b09b
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-11-20 19:39:50 +0530

    Refractored doctor dashboard

--- src/components/Doctor/DoctorDashboard.jsx
+++ src/components/Doctor/DoctorDashboard.jsx
@@ -9,7 +9,8 @@ import Grid from '@mui/material/Grid2';
 import Appointments from './Appointments';
 import PatientModule from './PatientModule';
 import PrescriptionTable from './PrescriptionTable';
-import { Box, Button, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Grid2, Typography } from '@mui/material';
+
 
 
 const NAVIGATION = [
@@ -77,15 +78,26 @@ export default function DoctorDashboard(props) {
             <Grid container spacing={1}>
               <Grid size={5} />
               <Grid size={12}>
+                <Box display="flex" gap={2}>
+                  <Typography variant='h5' className=''>Status:</Typography>
+                  <Typography variant='h5' className='text-danger'>Offline</Typography>
+                </Box>
                 <Appointments />
-                <Container  className='mt-3'>
-                <Typography textAlign="center" variant='h6'>Go Offline</Typography>
-                  <Box 
-                  display="flex"
-                  justifyContent="center"
-                  >
-                    <Button variant='contained'>Go Offline</Button>
-                  </Box>
+                <Container >
+                  <Grid2 container className='mt-3 d-flex justify-content-center' spacing={2}>
+                    <Grid2 size={3}>
+                      <Typography textAlign="center" variant='h6'>Go Pause</Typography>
+                      <Box display="flex" justifyContent="center">
+                        <Button style={{background: "#DE3163	"}} variant='contained'>Pause</Button>
+                      </Box>
+                    </Grid2>
+                    <Grid2 size={3}>
+                      <Typography textAlign="center" variant='h6'>Go Offline</Typography>
+                      <Box display="flex" justifyContent="center">
+                        <Button variant='contained'>Offine</Button>
+                      </Box>
+                    </Grid2>
+                  </Grid2>
                 </Container>
               </Grid>
               <Grid size={12}>
